4d7a318cd00162a1d072b30800e4355ebb6ea4d2
mehrabriyan <mehraabriyan@gmail.com>
2022-06-24 18:26:13 +0600

    app init done

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { notFound , errorHandler } = require('./middlewares/errorMiddlewares');
+const dbconnect = require('./dbconnect');
+
+
+const app = express();
+
+
+//db connection
+dbconnect();
+
+//external middlewares
+app.use(cors());
+dotenv.config();
+
+//Internal middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//routes
+
+
+app.get('/', (req, res) => {
+    res.send('server is online');
+})
+
+//bad request
+app.get('*', (req, res) => {
+    res.status(404).json({
+        message: 'Page not found'
+    });
+})
+
+//error handling
+app.use(notFound)
+app.use(errorHandler)
+
+
+// listing to the server
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
+})
